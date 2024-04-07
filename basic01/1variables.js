fe2298170fe0bb4a68fa704f77453388c2000896
Jaydeepkumar0p <118693035+Jaydeepkumar0p@users.noreply.github.com>
2024-04-07 15:05:45 +0000

    container config

---
+++ basic01/1variables.js
@@ -0,0 +1,7 @@
+const accountId=144553
+let accountEmail="jaideepkr.googl.com"
+var accountPassword="12345"
+accountCity="jaipur"
+console.table([accountEmail,accountId,accountPassword,accountCity])
+let accountSTate;
+console.log(accountSTate)
